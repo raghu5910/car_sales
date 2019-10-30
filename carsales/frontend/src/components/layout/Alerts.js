b944a673ed8b3c7afd076a594f7c2103f0363499
Raghu Paredla <raghuram5910@gmail.com>
2019-10-30 13:31:04 +0530

    Token.objects.create returns a tuple

--- carsales/frontend/src/components/layout/Alerts.js
+++ carsales/frontend/src/components/layout/Alerts.js
@@ -20,6 +20,11 @@ export class Alerts extends Component {
         alert.error(error.msg.non_field_errors.join());
       if (error.msg.username) alert.error(error.msg.username.join());
     }
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
+    }
   }
 
   render() {
